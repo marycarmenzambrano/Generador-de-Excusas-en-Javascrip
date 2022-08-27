a103569c9b974afaeb8ca8aa804f30d266cd569e
Marycarmen Zambrano <zambranomarycarmen48@gmail.com>
2022-08-27 00:12:26 +0000

    se agrega funcion

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
-function generate(){
+//function generate(){
     let quien = ["el Perro", "mi Abuela", "su Tortuga", "mi pájaro"];
     let que = ["comer", "molesta", "aplastada", "roto", ];
     let cuando = ["antes de la clase", "justo a tiempo", "cuando terminé", "durante mi almuerzo", "mientras estaba orando"];
 
-    let rand_quien = Math.floor(Math.random() * quien.length);
-    let rand_que = Math.floor(Math.random() * que.length);
-    let rand_cuando = Math.floor(Math.random() * cuando.length); 
+    //let rand_quien = Math.floor(Math.random() * quien.length);
+    //let rand_que = Math.floor(Math.random() * que.length);
+    //let rand_cuando = Math.floor(Math.random() * cuando.length); 
  
+//}
+
+//console.log(quien[1] + " "+ que[3] + " "+ cuando[1]) 
+
+//console.log(quien[Math.floor(Math.random() * quien.length)])
+
+//console.log(quien[Math.floor(Math.random() * quien.length)] + " "+ que[Math.floor(Math.random() * que.length)] + " "+ cuando[Math.floor(Math.random() * cuando.length)])
+
+function generateExcuse (quiennArray, queArray, cuandoArray){
+    let cuandoRandon = Math.floor(Math.random() * cuandoArray.length);
+    let queRandon = Math.floor(Math.random() * queArray.length);
+    let quienRandon = Math.floor(Math.random() * quien.length);
+
+    return quien[quienRandon] + " "+ que[queRandon] + " "+ cuando[cuandoRandon]
 }
 
-console.log(Math.floor(Math.random() * 10))
+let resultado = generateExcuse(quien, que, cuando);
+console.log(resultado)
 
 
-//ocument.getElementById("result").innerHTML = "<h1>Nombre:</h1><div classe='alert alert-success'><h2>"+quien[rand_quien] + " " +que[rand_que]+ " "+cuando[rand_cuando]+"</h2></div>";
+//document.getElementById("result").innerHTML = "<h1>Nombre:</h1><div classe='alert alert-success'><h2>"+quien[rand_quien] + " " +que[rand_que]+ " "+cuando[rand_cuando]+"</h2></div>";
